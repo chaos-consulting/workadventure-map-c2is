08953b41cf8de3b857c248af0db0365f9fde8fa2
Skymaker <lukas@skymaker.info>
2021-12-27 14:02:35 +0100

    fix issues with rc3 world

--- dev0.js
+++
@@ -1,7 +0,0 @@
-//WA.chat.sendChatMessage('Hello world', 'Mr Robot');
-//WA.player.onPlayerMove(callback: HasPlayerMovedEventCallback): void;
-WA.player.onPlayerMove(console.log);
-
-//HasPlayerMovedEventCallback(){
-
-//}
